ebe6ebf8ab7755ea4584df36ee7a1d5e2a43aaf5
Aksa George <aksajeeku@gmail.com>
2021-09-29 14:50:54 -0400

    All routes created and views for each routes created

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,17 @@ var router = express.Router();
 router.get('/', function (req, res) {
   res.render('index');
 });
+router.get('/about', (req, res) => {
+  res.render('about');
+})
+router.get('/projects', (req, res) => {
+  res.render('project');
+})
+router.get('/services', (req, res) => {
+  res.render('service');
+})
+router.get('/contact', (req, res) => {
+  res.render('contact');
+})
 
 module.exports = router;
