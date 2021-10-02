642af663c60a1d01f3acf0216b5a20cb91d10a10
Aksa George <aksajeeku@gmail.com>
2021-10-02 16:05:26 -0400

    Site fully completed....launced

--- routes/index.js
+++ routes/index.js
@@ -2,19 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 
-router.get('/', function (req, res) {
+router.get('/', function (req, res, next) {
   res.render('index');
 });
-router.get('/about', (req, res) => {
+router.get('/about', (req, res, next) => {
   res.render('about');
 })
-router.get('/projects', (req, res) => {
+router.get('/projects', (req, res, next) => {
   res.render('project');
 })
-router.get('/services', (req, res) => {
+router.get('/services', (req, res, next) => {
   res.render('service');
 })
-router.get('/contact', (req, res) => {
+router.get('/contact', (req, res, next) => {
   res.render('contact');
 })
 
