812f3368b914ad5fc7450932334d9bd5723897d6
Aksa George <aksajeeku@gmail.com>
2021-10-06 18:46:03 +0530

    Login page created

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,28 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-
-router.get('/', function (req, res, next) {
-  res.render('index');
+router.get("/", function (req, res, next) {
+  res.render("index");
 });
-router.get('/about', (req, res, next) => {
-  res.render('about');
-})
-router.get('/projects', (req, res, next) => {
-  res.render('project');
-})
-router.get('/services', (req, res, next) => {
-  res.render('service');
-})
-router.get('/contact', (req, res, next) => {
-  res.render('contact');
+router.get("/about", (req, res, next) => {
+  res.render("about");
+});
+router.get("/projects", (req, res, next) => {
+  res.render("project");
+});
+router.get("/services", (req, res, next) => {
+  res.render("service");
+});
+router.get("/contact", (req, res, next) => {
+  res.render("contact");
+});
+
+router.get("/login", (req, res, next) => {
+  res.render("login");
+});
+router.post('/login',(req,res,next)=>{
+  let userData = req.body;
+  // console.log(userData);
 })
 
 module.exports = router;
